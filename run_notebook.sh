4575650d6029789b3411211fa9d51a674953c6d3
Hélain Schoonjans <helain.schoonjans@eurekanetwork.org>
2018-03-21 13:54:14 +0100

    Did the first exercise

---
+++ run_notebook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run on windows
+# NOTE: you could want to copy the command and paste it in a terminal instead of running this file.
+docker run -it --rm -p 8888:8888 -v /c/Users/hes/Documents/Notebooks/TextMining:/home/jovyan/work heschoon/nlp-graphs-notebook
